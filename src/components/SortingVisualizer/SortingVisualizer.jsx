09b467da42af414297332639cee5e50a551a39b4
pasqualeorlando <76597311+pasqualeorlando@users.noreply.github.com>
2023-10-27 16:24:24 +0200

    Added selectionSort and bubbleSort animations

--- src/components/SortingVisualizer/SortingVisualizer.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -1,10 +1,18 @@
 import React, { useCallback, useEffect, useState } from "react";
 import "./SortingVisualizer.css";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
+import {
+  swap,
+  selectionSort,
+  bubbleSort,
+} from "../../sortingAlgorithms/sortingAlgorithms";
+import { pause } from "../../helper";
 
 function SortingVisualizer() {
   const [array, setArray] = useState([]);
   const { height } = useWindowDimensions();
+  const [sortAlgorithm, setSortAlgorithm] = useState("bubbleSort");
+  const [animationFinished, setAnimationFinished] = useState(true);
 
   const generateNewArray = useCallback(
     (length) => {
@@ -23,25 +31,62 @@ function SortingVisualizer() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function sort() {
-    const newArray = [];
+  function getAnimations() {
+    if (sortAlgorithm === "selectionSort") return selectionSort(array);
+    if (sortAlgorithm === "bubbleSort") return bubbleSort(array);
+  }
 
-    array.forEach((value) => {
-      newArray.push(value);
+  async function sortHelper() {
+    const animations = getAnimations();
+
+    setAnimationFinished(false);
+
+    animations.forEach((animation, index) => {
+      const [a, b, isSwap] = animation;
+
+      setTimeout(async () => {
+        const arrayBars = document.getElementsByClassName("arrayBar");
+        if (!isSwap) {
+          arrayBars[animation[0]].style.backgroundColor = "red";
+          arrayBars[animation[1]].style.backgroundColor = "red";
+        }
+
+        setArray((prevArray) => {
+          const newArray = [...prevArray];
+          if (isSwap) {
+            swap(newArray, a, b);
+          }
+          return newArray;
+        });
+
+        await pause(100);
+        /*if (
+          index + 1 < animations.length &&
+          animations[index + 1][0] !== animation[0]
+        )*/
+        arrayBars[animation[0]].style.backgroundColor = "blue";
+        arrayBars[animation[1]].style.backgroundColor = "blue";
+      }, index * 100);
     });
-    newArray.sort((a, b) => a - b);
 
-    setArray(newArray);
+    setAnimationFinished(true);
   }
 
   useEffect(() => {
-    generateNewArray(100);
+    generateNewArray(10);
   }, [generateNewArray]);
 
   return (
     <>
-      <button onClick={() => generateNewArray(100)}>Generate new array</button>
-      <button onClick={() => sort()}>Sort</button>
+      <button
+        onClick={() => generateNewArray(10)}
+        disabled={!animationFinished}
+      >
+        Generate new array
+      </button>
+      <button onClick={() => sortHelper()} disabled={!animationFinished}>
+        Sort
+      </button>
       <div className="arrayContainer">
         {array.map((element, idx) => {
           return (
---
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -0,0 +1,47 @@
+export function swap(array, idx1, idx2) {
+  const tmp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = tmp;
+}
+
+export function selectionSort(array) {
+  const animations = []; //array of array [i, j, swap or not]
+  const newArray = [...array];
+  let minIdx = 0;
+  for (let i = 0; i < newArray.length; i++) {
+    //search for minimum in subarray
+    minIdx = i;
+    for (let j = i + 1; j < newArray.length; j++) {
+      if (newArray[minIdx] > newArray[j]) {
+        minIdx = j;
+      }
+      animations.push([i, j, false]);
+    }
+    animations.push([i, minIdx, true]);
+    swap(newArray, i, minIdx);
+  }
+
+  return animations;
+}
+
+export function bubbleSort(array) {
+  const animations = [];
+  const newArray = [...array];
+  let swapped;
+
+  for (let i = 0; i < newArray.length - 1; i++) {
+    swapped = false;
+    for (let j = 0; j < newArray.length - i - 1; j++) {
+      animations.push([j, j + 1, false]);
+      if (newArray[j] > newArray[j + 1]) {
+        swap(newArray, j, j + 1);
+        swapped = true;
+        animations.push([j, j + 1, true]);
+      }
+    }
+
+    if (!swapped) break;
+  }
+
+  return animations;
+}
